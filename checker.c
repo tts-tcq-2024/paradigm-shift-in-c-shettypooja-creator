fc574553075fb2c3bc65f5cd9100dc0619ad6493
shettypooja-creator <poojash2000@icloud.com>
2024-10-17 10:46:25 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,27 +1,16 @@
-#include <stdio.h>
+#include <stdio.h> // Include this for printf
 #include <assert.h>
+#include "battery.h"
 
-int checkRange(float value, float min, float max, const char* message) 
-{
-    if(value < min || value > max) 
-    {
-        printf("%s out of range!\n", message);
-        return 0;
-    }
-    return 1;
-}
+int main() {
+    // Test cases covering all scenarios
+    assert(batteryIsOk(25, 70, 0.7));   // All parameters in range
+    assert(!batteryIsOk(50, 85, 0.7));  // Temperature and SoC out of range
+    assert(batteryIsOk(2, 22, 0.1));    // Values near lower limit but still OK
+    assert(batteryIsOk(44, 79, 0.79));  // Values near upper limit but still OK
+    assert(!batteryIsOk(-1, 19, 0.81)); // All parameters out of range
 
-int batteryIsOk(float temperature, float soc, float chargeRate) 
-{
-    int returnVal = 1;
-    returnVal &= checkRange(temperature, 0, 45, "Temperature");
-    returnVal &= checkRange(soc, 20, 80, "State of Charge");
-    returnVal &= checkRange(chargeRate, 0, 0.8, "Charge Rate");
-    return returnVal;
-}
+    printf("All test cases passed!\n");
 
-int main() 
-{
-    assert(batteryIsOk(25, 70, 0.7));  
-    assert(!batteryIsOk(50, 85, 0));   
+    return 0;
 }
